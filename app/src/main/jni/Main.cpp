c81efd4605985b23dbb186f35994018c81438504
freeeismail-a11y <freeeismail@gmail.com>
2026-08-18 20:32:49 +0100

    Update Main.cpp

--- app/src/main/jni/Main.cpp
+++ app/src/main/jni/Main.cpp
@@ -1,48 +1,13 @@
-#include <jni.h>
-#include <string>
-#include <android/log.h>
-#include <ModMenu/Includes.h>
+bool aimbotEnabled = false;
+float fovValue = 90.0f;
+bool espDrawEnabled = false;
+bool noRecoilEnabled = false;
+bool noSpreadEnabled = false;
+bool fastReloadEnabled = false;
 
-bool g_AimbotHeadshot = false;
-bool g_ESP = false;
-bool g_NoRecoil = false;
-bool g_NoSpread = false;
-
-void Hack_Aimbot() {
-    if (!g_AimbotHeadshot) return;
-}
-
-void Hack_ESP(void *instance) {
-    if (!g_ESP) return;
-}
-
-void Hack_WeaponParams() {
-    if (g_NoRecoil) {
-    }
-    if (g_NoSpread) {
-    }
-}
-
-void *UpdateThread(void *) {
-    while (true) {
-        Hack_Aimbot();
-        Hack_WeaponParams();
-        sleep(1);
-    }
-    return NULL;
-}
-
-void *ModMenuThread(void *) {
-    // AddToggle("Aimbot Headshot", &g_AimbotHeadshot);
-    // AddToggle("ESP Draw", &g_ESP);
-    // AddToggle("No Recoil", &g_NoRecoil);
-    // AddToggle("No Spread", &g_NoSpread);
-    return NULL;
-}
-
-JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved) {
-    pthread_t pt;
-    pthread_create(&pt, NULL, UpdateThread, NULL);
-    pthread_create(&pt, NULL, ModMenuThread, NULL);
-    return JNI_VERSION_1_6;
-}
+AddToggle("Aimbot Headshot", &aimbotEnabled);
+AddSlider("Aimbot FOV", &fovValue, 1.0f, 180.0f);
+AddToggle("ESP Draw", &espDrawEnabled);
+AddToggle("No Recoil", &noRecoilEnabled);
+AddToggle("No Spread", &noSpreadEnabled);
+AddToggle("Fast Ammunition Loading", &fastReloadEnabled);
